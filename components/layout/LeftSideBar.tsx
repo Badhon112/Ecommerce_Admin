f547c734aa4d3ef917966c8bdad8ca782fda398f
badhon112 <biswasbadhon896@gmail.com>
2024-03-10 13:50:50 +0600

    Layout

--- components/layout/LeftSideBar.tsx
+++ components/layout/LeftSideBar.tsx
@@ -1,7 +1,33 @@
-import React from 'react'
+"use client"
+import { UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+import React from "react";
+
+import { navLinks } from "@/lib/constants";
+import { usePathname } from "next/navigation";
 
 export default function LeftSideBar() {
+  const pathName = usePathname();
   return (
-    <div>LeftSideBar</div>
-  )
+    <div className="h-screen left-0 static p-10 flex flex-col gap-16 bg-blue-2 shadow-lg max-lg:hidden">
+      <p className="text-[30px] font-semibold">PaikariBazar</p>
+      <div className="flex flex-col gap-12">
+        {navLinks.map((link) => (
+          <Link
+            href={link.url}
+            key={link.label}
+            className={`flex gap-4 text-body-medium ${
+              pathName === link.url ? "text-blue-1" : ""
+            }`}
+          >
+            {link.icon} <p>{link.label}</p>
+          </Link>
+        ))}
+      </div>
+      <div className="flex gap-4 text-body-medium items-center">
+        <UserButton />
+        <p>Edit Profile</p>
+      </div>
+    </div>
+  );
 }
--- components/layout/TopBar.tsx
+++ components/layout/TopBar.tsx
@@ -1,7 +1,48 @@
-import React from 'react'
+"use client";
+import { navLinks } from "@/lib/constants";
+import { UserButton } from "@clerk/nextjs";
+import { Menu } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 export default function TopBar() {
+  const [dropdownmenu, setDropdownmenu] = useState(false);
+  const pathName=usePathname()
   return (
-    <div>TopBar</div>
-  )
+    <div className="static top-0 z-20 w-full flex justify-between items-center px-8 py-4 bg-blue-2 lg:hidden">
+      <p className="text-[30px] font-semibold">PaikariBazar</p>
+      <div className="flex gap-8 max-md:hidden">
+        {navLinks.map((link) => (
+          <Link
+            href={link.url}
+            key={link.label}
+            className={`flex gap-4 text-body-medium ${pathName===link.url ?"text-blue-400":""}`}
+          >
+            <p>{link.label}</p>
+          </Link>
+        ))}
+      </div>
+      <div className="relative flex gap-4 items-center">
+        <Menu
+          className="cursor-pointer md:hidden"
+          onClick={() => setDropdownmenu(!dropdownmenu)}
+        />
+        {dropdownmenu && (
+          <div className="absolute top-10 right-6 flex flex-col gap-8 p-5 bg-white shadow-xl rounded-lg">
+            {navLinks.map((link) => (
+              <Link
+                href={link.url}
+                key={link.label}
+                className="flex gap-4 text-body-medium"
+              >
+                {link.icon} <p>{link.label}</p>
+              </Link>
+            ))}
+          </div>
+        )}
+        <UserButton />
+      </div>
+    </div>
+  );
 }
